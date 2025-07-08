f3816a0b8538a7ea01a8101dba4fe5c16bb68064
JeCAILLOUX <100211547+JeCailloux@users.noreply.github.com>
2025-07-08 10:32:46 +0200

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
-from app/app import create_app
+from app import create_app
 import os
-
-if __name__ == "__main__":
+if name == "main":
     port = int(os.getenv('PORT', 5002))
     app = create_app()
     app.run(host='0.0.0.0', port=port)
-
